5ec632c1bee1494d67c290a884a43835076fb847
LachlanD-K <lachlandufort-kennett@outlook.com>
2021-04-14 14:47:03 +0100

    Implemented plot function properly

--- src/plotting.js
+++ src/plotting.js
@@ -8,7 +8,47 @@ function getActivePlots () {
 	return activePlots;
 }
 
-const plot = function(container, data, options={}) {
-	const plotObject = new core.ResponsivePlot2D(container, options);
+function plot (container, dataObject, options={}) {
+	let plotObject;
+	const plotID = typeof container === "string" ? container : document.getElementById(container);
+	if (core.activeCanvases.hasOwnProperty(plotID)) {
+		plotObject = core.activeCanvases[plotID];
+	} else {
+		plotObject = new core.ResponsivePlot2D(container);
+	}
+	const optionList = [
+		"origin",
+		"backgroundCSS",
+		"xLims",
+		"yLims",
+		"majorTicks",
+		"minorTicks",
+		"majorGridlines",
+		"minorGridlines",
+		"majorTickSize",
+		"minorTickSize",
+		"majorGridSize",
+		"minorGridSize"
+	];
+	for (const option of optionList) {
+		if (options.hasOwnProperty(option)) {
+			const optionSetter = plotObject[`set${option.charAt(0).toUpperCase() + option.slice(1)}`];
+			if (optionSetter.length === 0) {
+				optionSetter.call(plotObject, ...(Array.isArray(options[option]) ? options[option] : [options[option]]));
+			} else {
+				optionSetter.call(plotObject, options[option]);
+			}
+		}
+	}
+	if (options.hasOwnProperty("reset") && options.reset === true) {
+		for (const id in plotObject.legend) {
+			if (plotObject.legend.hasOwnProperty(id)) {
+				plotObject.removeData(id);
+			}
+		}
+	}
+	if (dataObject !== null) {
+		plotObject.addData(dataObject.data, dataObject.id, dataObject.options);
+	}
 	return plotObject;
-};
+}
