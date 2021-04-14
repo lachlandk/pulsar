17587f5a0dab13a536dd92bb707ce847dcfa92b9
LachlanD-K <lachlandufort-kennett@outlook.com>
2021-04-14 12:10:24 +0100

    Implemented getActivePlots function

--- src/plotting.js
+++ src/plotting.js
@@ -1,4 +1,14 @@
-const plot = function(container, [...data], options={}) {
+function getActivePlots () {
+	const activePlots = {};
+	for (const canvasID in core.activeCanvases) {
+		if (core.activeCanvases[canvasID] instanceof core.ResponsivePlot2D) {
+			activePlots[canvasID] = core.activeCanvases[canvasID];
+		}
+	}
+	return activePlots;
+}
+
+const plot = function(container, data, options={}) {
 	const plotObject = new core.ResponsivePlot2D(container, options);
 	return plotObject;
-}
+};
