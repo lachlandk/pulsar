20dd8b8277ad4287f764b3904e3aab556fb9d5c9
LachlanD-K <lachlandufort-kennett@outlook.com>
2021-04-19 14:45:22 +0100

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,55 @@
-// TODO: node init
-// "devDependencies": {
-//   "del": "^5.1.0",
-//   "gulp": "^4.0.2",
-//   "gulp-inject": "^5.0.5",
-//   "gulp-rename": "^2.0.0",
-//   "gulp-strip-comments": "^2.5.2",
-//   "gulp-uglify": "^3.0.2",
-//   "uglify-es": "^3.3.9"
-// }
-
 const gulp = require("gulp");
-const rename = require("gulp-rename");
-const uglify = require("uglify-es");
-const strip = require("gulp-strip-comments");
-const inject = require("gulp-inject");
 const del = require("del");
-const composer = require("gulp-uglify/composer");
-const minify = composer(uglify, console);
-const modules = [
-];
+const replace = require("gulp-replace");
+const rename = require("gulp-rename");
+const strip = require("gulp-strip-comments");
+const terser = require("gulp-terser");
 
-async function clean(){
-	const paths = await del(["dist/node/**", "dist/web/**"])
-	console.log("Deleted old files/directories:\n", paths.join("\n"));
+async function clean() {
+	const paths = await del(["dist/web/**"]);
+	console.log(`Deleted old files:\n - ${paths.join("\n - ")}`);
 }
 
-function webify(){
-	return gulp.src("templates/web.js")
-		.pipe(inject(gulp.src(modules), {
-			starttag: "/* inject:js */",
-			endtag: "/* endinject */",
-			transform: function(filepath, file){
-				return file.contents.toString("utf-8");
+async function webify() {
+	const sourceFiles = await new Promise(resolve => {
+			const chunks = [];
+			const source = gulp.src("src/*.js");
+			source.on("data", chunk => chunks.push(chunk));
+			source.on("end", () => {
+				const files = [];
+				for (const chunk of chunks) {
+					files.push({path: chunk.path, contents: chunk.contents.toString()});
+				}
+				resolve(files);
+			});
+	});
+	const sourceString = (() => {
+		const sourceContents = [];
+		for (const file of sourceFiles) {
+			sourceContents.push(file.contents);
+		}
+		return sourceContents.join("\n");
+	})();
+	const sourceExports = (() => {
+		const globals = [];
+		for (const file of sourceFiles) {
+			for (const match of file.contents.matchAll(/^(?:const|function)\s([a-zA-Z_$][\w$]*)/gm)) {
+				globals.push(match[1]);
 			}
-		}))
+		}
+		const globalExports = [];
+		for (const global of globals) {
+			globalExports.push(`${globals.indexOf(global) === 0 ? "" : "\t\t"}${global}: ${global}${globals.indexOf(global) === globals.length - 1 ? "" : ","}`);
+		}
+		return globalExports.join("\n");
+	})();
+	return gulp.src("templates/web.js")
+		.pipe(replace("// gulp-inject library body", sourceString))
+		.pipe(replace("// gulp-inject library exports", sourceExports))
 		.pipe(strip())
 		.pipe(rename("pulsar-web.js"))
 		.pipe(gulp.dest("dist/web/"))
-		.pipe(minify())
+		.pipe(terser())
 		.pipe(rename({extname: ".min.js"}))
 		.pipe(gulp.dest("dist/web/"));
 }
