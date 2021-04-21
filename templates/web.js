d6d70b691055ad402ad473e8661b853e6a8746d0
LachlanD-K <lachlandufort-kennett@outlook.com>
2021-04-21 23:28:38 +0100

    Added documentation to web template

--- templates/web.js
+++ templates/web.js
@@ -16,6 +16,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ***/
+/**
+ * @namespace Pulsar
+ */
 window.Pulsar = (function () {
 // gulp-inject library body
 	return {
