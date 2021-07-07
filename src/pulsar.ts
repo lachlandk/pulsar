42a7eeb104a0d2afb6920607c9bd5be3227a42a4
LachlanD-K <lachlandufort-kennett@outlook.com>
2021-07-07 14:49:16 +0100

    Created pulsar.ts

---
+++ src/pulsar.ts
@@ -0,0 +1,10 @@
+import { ResponsiveCanvas, ResponsivePlot2D, activeCanvases } from "./core/index.js";
+
+export const core = {
+    ResponsiveCanvas: ResponsiveCanvas,
+    ResponsivePlot2D: ResponsivePlot2D,
+    activeCanvases: activeCanvases
+}
+
+export { Plot } from "./plotting/index.js";
+export { getActivePlots } from "./plotting/index.js";
