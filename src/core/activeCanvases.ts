d90962b9d41f9ce4fd5c3ab365cb8ef6b82bdfd6
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-06-06 14:42:02 +0100

    Deleted old files

--- src/core/activeCanvases.ts
+++
@@ -1,9 +0,0 @@
-import { ResponsiveCanvas } from "./ResponsiveCanvas.js";
-
-/**
- * Object containing the active canvas objects with their ID as the keys. It is used
- * internally by other objects.
- */
-export const activeCanvases: {
-    [id: string]: ResponsiveCanvas
-} = {};
