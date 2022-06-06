274cab56b7532e6344f954874e6b052875fdb7d0
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-06-06 14:35:06 +0100

    Added `PulsarObject` class

---
+++ src/core/PulsarObject.ts
@@ -0,0 +1,21 @@
+import { Pulsar } from "./Controller.js";
+import { CanvasContainer } from "./CanvasContainer.js";
+
+export class PulsarObject extends HTMLElement {
+    containers: CanvasContainer[] = []
+
+    constructor() {
+        super();
+        this.style.display = "grid";
+
+        Pulsar.activeObjects.push(this);
+    }
+
+    show() {
+        this.style.display = "grid";
+    }
+
+    hide() {
+        this.style.display = "none";
+    }
+}
