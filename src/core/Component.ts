5b76dad24d979d5547cd4f90d5f2dee4e4b553d7
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-06-06 14:39:19 +0100

    Created `Component` class

---
+++ src/core/Component.ts
@@ -0,0 +1,13 @@
+import { ResponsiveCanvas } from "./ResponsiveCanvas.js";
+
+export class Component {
+    canvas: ResponsiveCanvas
+    draw: (context: CanvasRenderingContext2D) => void
+
+    constructor(canvas: ResponsiveCanvas, draw: (context: CanvasRenderingContext2D) => void) {
+        this.canvas = canvas;
+        this.canvas.components.push(this);
+        this.canvas.updateFlag = true;
+        this.draw = draw;
+    }
+}
